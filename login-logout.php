2669e0b34a28a83d0ec1b5630cb80c1beda1fbb5
webvitaly <webvitaly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-05 15:31:55 +0000

    1.0.0 commit (Added show or hide register or site-admin link)
    git-svn-id: https://plugins.svn.wordpress.org/login-logout/trunk@393489 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- login-logout.php
+++ login-logout.php
@@ -2,12 +2,15 @@
 /*
 Plugin Name: Login Logout
 Plugin URI: http://web-profile.com.ua/wordpress/plugins/login-logout/
-Description: Show login or logout link.
-Version: 1.0.0
+Description: Show login or logout link. Show register or site-admin link.
+Version: 1.1.0
 Author: webvitaly
 Author Email: webvitaly(at)gmail.com
 Author URI: http://web-profile.com.ua/
 
+Future features:
+- http-s;
+- add translation;
 */
 
 class WP_Widget_Login_Logout extends WP_Widget {
@@ -19,6 +22,7 @@ class WP_Widget_Login_Logout extends WP_Widget {
 	
 	function widget( $args, $instance ) {
 		extract($args);
+		$register_admin = $instance['register_admin'] ? '1' : '0';
 		$title = apply_filters('widget_title', empty($instance['title']) ? __('Login Logout') : $instance['title'], $instance, $this->id_base);
 
 		echo $before_widget;
@@ -29,7 +33,11 @@ class WP_Widget_Login_Logout extends WP_Widget {
 ?>
 			<ul>
 				<li><?php wp_loginout( $return_link ); ?></li>
-				<?php wp_register(); ?>
+				<?php
+				if( $register_admin ){
+					wp_register();
+				}
+				?>
 			</ul>
 <?php
 		echo $after_widget;
@@ -37,27 +45,33 @@ class WP_Widget_Login_Logout extends WP_Widget {
 
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
+		$new_instance = wp_parse_args( (array) $new_instance, array( 'title' => '', 'register_admin' => 0) );
 		$instance['title'] = strip_tags($new_instance['title']);
-
+		$instance['register_admin'] = $new_instance['register_admin'] ? 1 : 0;
 		return $instance;
 	}
 
 	function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'register_admin' => 0 ) );
 		$title = strip_tags($instance['title']);
+		$register_admin = $instance['register_admin'] ? 'checked="checked"' : '';
 ?>
-			<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
+			<p>
+				<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+			</p>
+			<p>
+				<input class="checkbox" type="checkbox" <?php echo $register_admin; ?> id="<?php echo $this->get_field_id('register_admin'); ?>" name="<?php echo $this->get_field_name('register_admin'); ?>" /> <label for="<?php echo $this->get_field_id('register_admin'); ?>"><?php _e('Show register or site-admin link'); ?>;</label>
+			</p>
 <?php
 	}
 }
 
 
-/*
 function login_logout_widget_init() {
 	register_widget('WP_Widget_Login_Logout');
 	do_action('widgets_init');
 }
 add_action('init', 'login_logout_widget_init', 1);
-*/
 
-add_action('widgets_init', create_function('', 'return register_widget("WP_Widget_Login_Logout");'));
+//add_action('widgets_init', create_function('', 'return register_widget("WP_Widget_Login_Logout");'));
+
--- readme.txt
+++ readme.txt
@@ -5,8 +5,8 @@ Plugin URI: http://web-profile.com.ua/wordpress/plugins/login-logout/
 Tags: login, logout, widget, meta
 Author URI: http://web-profile.com.ua/wordpress/
 Requires at least: 3.0
-Tested up to: 3.1.2
-Stable tag: 1.0.0
+Tested up to: 3.2
+Stable tag: 1.1.0
 
 "Login Logout" plugin add widget with login or logout link.
 
@@ -14,17 +14,22 @@ Stable tag: 1.0.0
 
 "Login Logout" plugin add widget with login or logout link which you can add to your sidebar.
 = If user is not logged in there are two links: =
-* login (after login user will return to previous page);
-* register (if user can register);
+* login (after login action user will return to previous page);
+* register (if user can register) (if checkbox is active);
 
 = If user is logged in there are two links: =
-* logout (after logout user will return to previous page);
-* site admin;
+* logout (after logout action user will return to previous page);
+* site admin (if checkbox is active);
+
+[Login-Logout plugin page](http://web-profile.com.ua/wordpress/plugins/login-logout/)
 
 [WordPress stuff](http://web-profile.com.ua/wordpress/)
 
 
 == Changelog ==
+
+= 1.1.0 =
+* Added show or hide register or site-admin link;
 
 = 1.0.0 =
 * Initial release;
